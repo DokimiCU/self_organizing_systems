39703690936f73af4ad6a56e920a772ae0ac1445
DokimiCU <DokimiCU@gmail.com>
2017-05-16 13:00:47 +1200

    Update init.lua

--- ecobots/init.lua
+++ ecobots/init.lua
@@ -6,7 +6,7 @@ dofile(minetest.get_modpath("ecobots").."/nodes.lua")
 
 dofile(minetest.get_modpath("ecobots").."/craft.lua")
 
-dofile(minetest.get_modpath("ecobots").."/spawner.lua")
+---dofile(minetest.get_modpath("ecobots").."/spawner.lua")
 
 dofile(minetest.get_modpath("ecobots").."/photosynth_bot.lua")
 

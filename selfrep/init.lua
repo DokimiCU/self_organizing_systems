1cb9167c6bf855eaa82633d47319cf6054e34b56
DokimiCU <DokimiCU@gmail.com>
2018-01-13 17:00:44 +1300

    Update init.lua

--- selfrep/init.lua
+++ selfrep/init.lua
@@ -11,6 +11,12 @@ dofile(minetest.get_modpath("selfrep").."/road.lua")
 
 dofile(minetest.get_modpath("selfrep").."/tower.lua")
 
+dofile(minetest.get_modpath("selfrep").."/sinkhole.lua")
+
+dofile(minetest.get_modpath("selfrep").."/tunnel.lua")
+
+dofile(minetest.get_modpath("selfrep").."/ladder.lua")
+
 
 
 
